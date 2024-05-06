e0ed972ac9b253e28bba7182739e3b8b8063ba94
Carol <email@exemplo.com>
2024-05-06 11:34:07 -0300

    add: estilização CSS

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
     const validacao = validaCampo(campoA, campoB);
     if(validacao === true) {
+        mensagem.classList.add('valid');
         mensagem.innerHTML = "Válido! O número do campo B é maior que o campo A."
-        mensagem.style.color = "green";
         limpaCampo();
     } else {
+        mensagem.classList.add('invalid');
         mensagem.innerHTML = "Inválido! O campo B precisa ser maior que o campo A."
-        mensagem.style.color = "red";
     }
 })
