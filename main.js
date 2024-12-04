fd67f354c0eeb4fbd6f9968903fe2b8b279edf2a
Carol <email@exemplo.com>
2024-12-04 16:12:28 -0300

    exercicio concluido

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (num1, num2) => num1 * num2;
+console.log(multiplicar(5, 8));
+function saudar(nome) {
+    return `Olá, ${nome}!`;
+}
+console.log(saudar('Carol'));
---
+++ main.ts
@@ -0,0 +1,9 @@
+const multiplicar = (num1: number, num2: number) => num1 * num2;
+
+console.log(multiplicar(5, 8));
+
+function saudar(nome: string): string {
+    return `Olá, ${nome}!`;
+}
+
+console.log(saudar('Carol'));
