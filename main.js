eae4d31c52bc3e72afef5ea7ab0244539ce6bc6e
Carol <email@exemplo.com>
2024-12-04 16:13:46 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 const multiplicar = (num1, num2) => num1 * num2;
-console.log(multiplicar(5, 8));
+console.log(multiplicar(3, 9));
 function saudar(nome) {
     return `Olá, ${nome}!`;
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 const multiplicar = (num1: number, num2: number) => num1 * num2;
 
-console.log(multiplicar(5, 8));
+console.log(multiplicar(3, 9));
 
 function saudar(nome: string): string {
     return `Olá, ${nome}!`;
